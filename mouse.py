03536ec55589682b5b8d0c7d0e5a5b72c8dab6ae
iceflinger <ice@loud.house>
2025-08-01 18:44:46 -0400

    Feels good now

--- mouse.py
+++ mouse.py
@@ -3,18 +3,21 @@ import math
 from nxbt import nxbt
 
 pygame.init()
-screen = pygame.display.set_mode((1200,1200))
+
+screen_size = 500
+
+screen = pygame.display.set_mode((screen_size,screen_size))
 clock = pygame.time.Clock()
 running = True
 
-mouse_sens_x = 80
-mouse_sens_y = 80
-#"BC:9E:BB:F8:71:8C"
+mouse_sens_x = 50
+mouse_sens_y = 75
+gyro_accel_scaling = 40 #change this if you change mouse_sense_y maybe
+
 switch_addr = ""
 
 nx = nxbt.Nxbt()
 index = nx.create_controller(nxbt.PRO_CONTROLLER, reconnect_address=switch_addr)
-
 
 print("Connecting controller to switch... (Go to Change Grip/Order and trust the switch from bluetoothctl on PC)")
 nx.wait_for_connection(index)
@@ -26,14 +29,21 @@ packet["IMU_DATA"] = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
 framecount = 0
 
 pygame.event.set_grab(True)
+pygame.mouse.set_visible(False)
 
-#idk if this even helps tbh
-jitter = 3
+camera_y = 0
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+    if pygame.mouse.get_focused():
+        pygame.mouse.set_visible(False)
+        pygame.event.set_grab(True)
+    else:
+        pygame.mouse.set_visible(True)
+        pygame.event.set_grab(False)
 
     if pygame.key.get_pressed()[pygame.K_ESCAPE]:
         running = False
@@ -97,10 +107,14 @@ while running:
         packet['X'] = True
 
     if pygame.key.get_pressed()[pygame.K_z]:
-        pygame.mouse.set_visible(False)
-        pygame.mouse.set_pos([600,600])
-        pygame.mouse.set_visible(True)
+        camera_y = 0
         packet['Y'] = True
+
+    if pygame.key.get_pressed()[pygame.K_x]:
+        packet['HOME'] = True
+
+    if pygame.key.get_pressed()[pygame.K_c]:
+        packet['CAPTURE'] = True
 
     if pygame.key.get_pressed()[pygame.K_r]:
         packet['MINUS'] = True
@@ -110,36 +124,31 @@ while running:
 
     screen.fill("purple")
 
-
-    null, ny = pygame.mouse.get_pos()
-
     dx, dy = pygame.mouse.get_rel()
+
+    camera_y = camera_y - dy
 
     val_pitch = (0+(dy*mouse_sens_y))%0xFFFF
     val_yaw = (0-(dx*mouse_sens_x))%0xFFFF
-
+    
     #vaguely scale the y position of the cursor with the accepted acc values of the IMU
-    val_acc = (-(math.floor((-600+ny)*(2))))-jitter
-    if jitter != -3:
-        jitter = -3
-    else:
-        jitter = 3
-    print(ny)
+    val_acc = math.floor(camera_y*(mouse_sens_y/gyro_accel_scaling))
+
     # Forward-Back axis accelerometer
     packet['IMU_DATA'][0+((framecount%3)*12)] = val_acc & 0xFF
     packet['IMU_DATA'][1+((framecount%3)*12)] = (val_acc >> 8) & 0xFF
 
     # Left-Right axis accelerometer
-    packet['IMU_DATA'][2+((framecount%3)*12)] = jitter & 0xFF
-    packet['IMU_DATA'][3+((framecount%3)*12)] = (jitter >> 8) & 0xFF
+    packet['IMU_DATA'][2+((framecount%3)*12)] = 0 & 0xFF
+    packet['IMU_DATA'][3+((framecount%3)*12)] = (0 >> 8) & 0xFF
 
     # Up-Down axis accelerometer
-    packet['IMU_DATA'][4+((framecount%3)*12)] = 4100+jitter-abs(math.floor(val_acc/2)) & 0xFF
-    packet['IMU_DATA'][5+((framecount%3)*12)] = (4100+jitter-abs(math.floor(val_acc/2)) >> 8) & 0xFF
+    packet['IMU_DATA'][4+((framecount%3)*12)] = 4100-abs(math.floor(val_acc/2)) & 0xFF
+    packet['IMU_DATA'][5+((framecount%3)*12)] = (4100-abs(math.floor(val_acc/2)) >> 8) & 0xFF
 
     # Up-Down Gyroscope
-    packet['IMU_DATA'][8+((framecount%3)*12)] = val_pitch+jitter & 0xFF
-    packet['IMU_DATA'][9+((framecount%3)*12)] = (val_pitch+jitter >> 8) & 0xFF
+    packet['IMU_DATA'][8+((framecount%3)*12)] = val_pitch & 0xFF
+    packet['IMU_DATA'][9+((framecount%3)*12)] = (val_pitch >> 8) & 0xFF
 
     # Left-Right Gyroscope
     packet['IMU_DATA'][10+((framecount%3)*12)] = val_yaw & 0xFF
@@ -150,14 +159,8 @@ while running:
         nx.set_controller_input(index, packet)
         packet = nx.create_input_packet()
         packet["IMU_DATA"] = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-        if pygame.key.get_focused():
-            pygame.mouse.set_visible(False)
-            pygame.mouse.set_pos([600,ny])
-            pygame.mouse.set_visible(True)
-            pygame.mouse.get_rel()
 
     framecount = framecount+1
-    clock.tick(120)
+    clock.tick(133)
 
 pygame.quit()
-
